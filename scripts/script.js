12ab5a28614862fd1f86153c1101f3f45a3771a4
Deepak Balaraman <deepak22deepak22@gmail.com>
2022-01-30 13:12:22 +0530

    Added Animated BG

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+//Sticky header upon scrolling
 let nav = document.querySelector("nav");
 let val;
 window.onscroll = function() {
@@ -9,6 +10,7 @@ window.onscroll = function() {
 
 }
 
+//Show or Hide menu icon upon menu open/close
 let body = document.querySelector("body");
 let header = document.querySelector(".header");
 let menuBtn = document.querySelector(".menu-btn");
@@ -26,24 +28,40 @@ cancelBtn.onclick = function(){
   body.style.overflow = "auto";
 }
 
+//Close menu upon selecting an option
 let navLinks = document.querySelectorAll(".menu li a");
 for (var i = 0; i < navLinks.length; i++) {
   navLinks[i].addEventListener("click" , function() {
     header.classList.remove("active");
     menuBtn.style.opacity = "1";
     menuBtn.style.pointerEvents = "auto";
+    body.style.overflow = "auto";
   });
 }
 
+VANTA.WAVES({
+  el: "#Home",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 0.65,
+  scaleMobile: 0.65,
+  color: 0x003179,
+  shininess: 7.00,
+  waveHeight: 14.00,
+  waveSpeed: 1.50,
+  zoom: 0.65
+});
 
-//Fetch Live Subscribers Count
-
+//Fetch Live Subscribers Count and Latest Videos
 let apiKey = config.ytda;
 const channelID = config.ytid;
 const plistID = config.ytps;
 const subCount = document.querySelector('.subCount');
 var vidTitle = document.getElementsByClassName('vidTitle');
-var thumb = document.getElementsByClassName('vids');
+var thumb = document.getElementsByClassName('thumbs');
 
 let getSubscribers = () => {
     fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelID}&key=${apiKey}`)
